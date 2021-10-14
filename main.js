ee8ffffe327666fe1088b13ee7fcaebbc27be206
NikolesM <nic2106@hotmail.com>
2021-10-14 12:37:10 -0500

    Se agrego el prototipado del proyecto para posibles mejoras

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+document.querySelector('.menu-btn').addEventListener('click', ()=>{
+    /*console.log('Click!!!') mensaje de consola para verificar si esta funcionando*/
+    /*invocamos el documento seleccionamos la clase nav-,menu
+    hacemos lista de clases y con el metodo toggle decimos que nos muestre el la lista cuando este se presione*/
+
+    document.querySelector('.nav-menu').classList.toggle('show');
+
+}) /*invocamos el documentos document, el selector, y la clase llamada .menu-btn */
+
+
+
+
